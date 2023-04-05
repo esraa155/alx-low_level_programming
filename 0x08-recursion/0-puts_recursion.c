7b97dfebfe87089828a5b10063fbb898657e5a8d
AhmedShahine1 <102306170+AhmedShahine1@users.noreply.github.com>
2023-04-05 17:18:36 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,23 +1,23 @@
-#include "stdio.h"
+#include "main.h"
 
-void _puts_recursion(char *c)
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+
+void _puts_recursion(char *s)
 {
 
-	if (*c == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar(*c);
-		_puts_recursion(c + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
 }
-
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
-}
-
