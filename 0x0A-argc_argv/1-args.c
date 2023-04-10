a2a9c87de47fbdb04c720bbb40f162a8b605a0bd
Esraa Mohamed Abdel star <103047126+esraa155@users.noreply.github.com>
2023-04-11 00:38:51 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: arr args
  * Return: 0
  */
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
