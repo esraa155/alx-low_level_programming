a7d334e360c030957248e7fb0296521cb4552672
Esraa Mohamed Abdel star <103047126+esraa155@users.noreply.github.com>
2023-04-20 03:45:18 +0200

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _putchar(char c);
+int _putchar(char z);
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*d)(char *));
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 
@@ -17,7 +17,7 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+int (*get_op_func(char *c))(int, int);
 
 #endif
 
